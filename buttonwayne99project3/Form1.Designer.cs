01c61e658a061dcadafbc43333aa780d1229ba83
aubrcart224 <aubrcart224@ed.amdsb.ca>
2021-04-23 15:09:56 -0400

    Add project files.

---
+++ buttonwayne99project3/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿
+namespace buttonwayne99project3
+{
+    partial class variablepractice
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.hockey = new System.Windows.Forms.Button();
+            this.Area = new System.Windows.Forms.Button();
+            this.carpet = new System.Windows.Forms.Button();
+            this.bill = new System.Windows.Forms.Button();
+            this.label = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // hockey
+            // 
+            this.hockey.Location = new System.Drawing.Point(2, 12);
+            this.hockey.Name = "hockey";
+            this.hockey.Size = new System.Drawing.Size(75, 59);
+            this.hockey.TabIndex = 0;
+            this.hockey.Text = "Hockey";
+            this.hockey.UseVisualStyleBackColor = true;
+            this.hockey.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Area
+            // 
+            this.Area.Location = new System.Drawing.Point(2, 100);
+            this.Area.Name = "Area";
+            this.Area.Size = new System.Drawing.Size(75, 68);
+            this.Area.TabIndex = 1;
+            this.Area.Text = "Area";
+            this.Area.UseVisualStyleBackColor = true;
+            this.Area.Click += new System.EventHandler(this.Area_Click);
+            // 
+            // carpet
+            // 
+            this.carpet.Location = new System.Drawing.Point(2, 201);
+            this.carpet.Name = "carpet";
+            this.carpet.Size = new System.Drawing.Size(75, 63);
+            this.carpet.TabIndex = 2;
+            this.carpet.Text = "Carpet";
+            this.carpet.UseVisualStyleBackColor = true;
+            this.carpet.Click += new System.EventHandler(this.carpet_Click);
+            // 
+            // bill
+            // 
+            this.bill.Location = new System.Drawing.Point(2, 312);
+            this.bill.Name = "bill";
+            this.bill.Size = new System.Drawing.Size(75, 73);
+            this.bill.TabIndex = 3;
+            this.bill.Text = "Bill";
+            this.bill.UseVisualStyleBackColor = true;
+            this.bill.Click += new System.EventHandler(this.bill_Click);
+            // 
+            // label
+            // 
+            this.label.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.label.Location = new System.Drawing.Point(152, 12);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(389, 380);
+            this.label.TabIndex = 4;
+            // 
+            // variablepractice
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkGray;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label);
+            this.Controls.Add(this.bill);
+            this.Controls.Add(this.carpet);
+            this.Controls.Add(this.Area);
+            this.Controls.Add(this.hockey);
+            this.Name = "variablepractice";
+            this.Text = "Variables Pratice";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button hockey;
+        private System.Windows.Forms.Button Area;
+        private System.Windows.Forms.Button carpet;
+        private System.Windows.Forms.Button bill;
+        private System.Windows.Forms.Label label;
+    }
+}
+
---
+++ buttonwayne99project3/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace buttonwayne99project3
+{
+    public partial class variablepractice : Form
+    {
+        public variablepractice()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string playerName = "Wayne Gretzky";
+            int playerNumber = 99;
+            label.Text = $" {playerName} is number {playerNumber}";
+        }
+
+        private void Area_Click(object sender, EventArgs e)
+        {
+            double radius = 15;
+            double pi = 3.14;
+            double area = 706.85;
+            label.Text = $" The area of a circle with a radius of {radius}cm is {area}cm^2";
+
+        }
+
+        private void carpet_Click(object sender, EventArgs e)
+        {
+            double length = 8.5;
+            double width = 6;
+            double costPerMeter = 19.95;
+            double area = length * width;
+            double totalCost = costPerMeter * area;
+            label.Text = $"The area of a room with demensions {length} x {width} is {area}^2";
+            label.Text += $"\n The cost to carpet this area at {costPerMeter} per square metre is ${totalCost}";
+        }
+
+        private void bill_Click(object sender, EventArgs e)
+        {
+            double shirtPrice = 12.49;
+            double total = 12.49 * 1.13;
+            double tax = total - shirtPrice;
+            double payedWith = 20.00;
+            double change = payedWith - total;
+            label.Text = "Bill of sale";
+            label.Text += $"\n\nShirt:   {shirtPrice}";
+            label.Text += $"\n\nTax:     {tax}";
+            label.Text += $"\nTotal:     {total}";
+            label.Text += $"\n\nTendered:{payedWith}";
+            label.Text += $"\nChange:    {change}";
+
+        }
+    }
+}
